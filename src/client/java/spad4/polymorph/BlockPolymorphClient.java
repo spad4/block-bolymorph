ff453d4696e10294eeed18c2ee3bc13a4760a3d8
spad4 <davidaltman28@yahoo.com>
2025-05-18 19:37:32 -0400

    initial commit

---
+++ src/client/java/spad4/polymorph/BlockPolymorphClient.java
@@ -0,0 +1,10 @@
+package spad4.polymorph;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockPolymorphClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/spad4/polymorph/BlockPolymorphDataGenerator.java
@@ -0,0 +1,11 @@
+package spad4.polymorph;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlockPolymorphDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
